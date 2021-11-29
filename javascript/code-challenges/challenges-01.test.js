0ea5973c25325dd7b3ac75918d64a88d8e03caa7
jamiguel23 <josematthewmiguel@gmail.com>
2021-11-29 15:03:27 -0800

    finished challange 5

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -87,7 +87,10 @@ const addValues = (arr, value) => {
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  
+  for (let i=0; i<times; i++){
+    callback(arr,num);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
