774570dce8ec4e0ba8e71f6c8a8690b60d296ec5
jamiguel23 <josematthewmiguel@gmail.com>
2021-11-30 13:32:52 -0800

    challenge 1 passed

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -9,7 +9,12 @@ Write a function named raisedToTheThird that takes in an array of numbers and re
 ------------------------------------------------------------------------------------------------ */
 
 const raisedToTheThird = (arr) => {
-  // Solution code here...
+  let cubed = [];
+  arr.forEach(num => {
+    cubed.push(Math.pow(num,3));
+  }
+  );
+  return cubed;
 };
 
 /* ------------------------------------------------------------------------------------------------
