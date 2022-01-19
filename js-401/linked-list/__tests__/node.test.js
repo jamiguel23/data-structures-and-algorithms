43058b4158caf5453c797266453b2e410d72321e
jamiguel23 <josematthewmiguel@gmail.com>
2022-01-18 17:00:44 -0800

    linked list test

--- js-401/linked-list/__tests__/node.test.js
+++ js-401/linked-list/__tests__/node.test.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const Node = require('../linkedList.js');
+const Node = require('../linkedList');
 
 xdescribe('Node Test', () => {
-  it('should test the node contructor', () => {
+  it('should test the node constructor', () => {
     let value = 'cookie';
     let node = new Node(value);
     expect(node.value).toEqual(value);
