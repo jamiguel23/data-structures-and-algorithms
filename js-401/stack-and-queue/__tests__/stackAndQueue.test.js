cf9785d345341abf02d3221eb4436c1a91c81d0c
jamiguel23 <josematthewmiguel@gmail.com>
2022-01-22 10:58:22 -0800

    added testing file for stack and queues

---
+++ js-401/stack-and-queue/__tests__/stackAndQueue.test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Queue = require('../stackAndQueue.js');
+const Stack = require('../stackAndQueue.js');
+const Node = require('../Node.js');
+const LinkedList = require('../LinkedList.js');
+
+let list = new LinkedList();
+let stack = new Stack();
+let val = 50;
+let newVal = 'foo';
+
+
+describe(' testing linkedList', () => {
+  it('should create linkedList and insert to the front', () => {
+    list.insert(newVal);
+    console.log(list);
+    expect(list.head.value).toEqual('foo');
+    expect(list.head.next).toEqual(null);
+  });
+});
+
+
+describe('testing stack', () =>{
+  it('create stack where top is null', () => {
+    expect(stack.top).toEqual(null);
+  });
+
+  it('should push newVal into stack', () => {
+
+    stack.push(newVal);
+    expect(stack.top.value).toEqual(list.head.value);
+    expect(list.head.value).toEqual('foo');
+    expect(list.head.next).toEqual(null);
+  });
+
+  it('should push val into stack, making it new top', () => {
+
+    stack.push(val);
+    expect(stack.top.value).toEqual(50);
+  });
+
+  it('should remove top value from stack', () => {
+
+    stack.pop();
+    // expect()
+  });
+});
+
