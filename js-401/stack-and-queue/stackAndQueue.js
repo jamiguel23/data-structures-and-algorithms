87f6745cbef97d9c537dd91536e4a85dc4b87df2
jamiguel23 <josematthewmiguel@gmail.com>
2022-01-21 17:26:24 -0800

    finished queue

--- js-401/stack-and-queue/stackAndQueue.js
+++ js-401/stack-and-queue/stackAndQueue.js
@@ -48,27 +48,87 @@ class Stack {
   }
 }
 
-let list = new LinkedList();
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+  }
 
-list.append('matt');
+  enqueue(value) {
+    let node = new Node(value);
+
+    if (this.front === null) {
+      this.front = node;
+      this.back = node;
+      return;
+    } else {
+      this.back.next = node;
+      this.back = node;
+      return;
+    }
+  }
+
+  dequeue() {
+
+    let nodeToRemove = this.front;
+    this.front = nodeToRemove.next;
+    nodeToRemove.next = null;
+    if (!this.front) {
+      this.back = null;
+    }
+    return nodeToRemove.value;
+  }
+
+  peek(){
+    if ( this.front === null){
+      return 'empty head';
+    } else {
+      return this.front.value;
+    }
+  }
+
+  isEmpty() {
+    if (this.front === null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
+
+let queue = new Queue();
+let newQueue = new Queue();
+
+queue.enqueue(5);
+queue.enqueue(10);
+queue.enqueue(15);
+
+console.log(JSON.stringify(queue));
+console.log('this is the head value', queue.peek());
+console.log(newQueue.peek());
+console.log(queue.isEmpty());
+// let list = new LinkedList();
+
+// list.append('matt');
 // list.append('miguel');
 // list.append('23');
 
 
 // console.log(JSON.stringify(list));
 
-let stack = new Stack();
-let newStack = new Stack();
+// let stack = new Stack();
+// let newStack = new Stack();
 
-stack.push('matt');
-stack.push('miguel');
-stack.push('10');
-stack.push('foo');
-let val = stack.pop();
+// stack.push('matt');
+// stack.push('miguel');
+// stack.push('10');
+// stack.push('foo');
+// let val = stack.pop();
 
 // console.log(JSON.stringify(stack), val);
 // console.log(stack.peek());
-console.log(newStack.peek());
-console.log(stack.isEmpty());
-console.log('new stack' , newStack);
-console.log(newStack.isEmpty());
+// console.log(newStack.peek());
+// console.log(stack.isEmpty());
+// console.log('new stack' , newStack);
+// console.log(newStack.isEmpty());
