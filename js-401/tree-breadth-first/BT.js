a9bda594aa147c514fa2e6a837be25980cec13bb
jamiguel23 <josematthewmiguel@gmail.com>
2022-02-01 14:07:03 -0800

    added breadth first

---
+++ js-401/tree-breadth-first/BT.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const Node = require('./Node.js');
+
+
+
+class BT {
+  constructor() {
+    this.root = null;
+  }
+
+  // root -> left -> right
+  preOrder() {
+
+    let helper = (node) => {
+      console.log(node.value);
+      if (node.left) helper(node.left);
+      if (node.right) helper(node.right);
+    };
+    helper(this.root);
+
+  }
+
+  // left -> root -> right
+  inOrder() {
+
+    let helper = (node) => {
+      if (node.left) helper(node.left);
+      console.log(node.value);
+      if (node.right) helper(node.right);
+    };
+    helper(this.root);
+
+  }
+
+  // left -> right -> root
+  postOrder() {
+
+    let helper = (node) => {
+      if (node.left) helper(node.left);
+      if (node.right) helper(node.right);
+      console.log(node.value);
+    };
+    helper(this.root);
+  }
+
+  findMax() {
+
+    let maxValue = 0;
+    // let res = [];
+    let walk = (node) => {
+      if (node.left) walk(node.left);
+      if (node.value > maxValue) {
+        maxValue = node.value;
+        // res.push(maxValue);
+      }
+      if (node.right) walk(node.right);
+    };
+    walk(this.root);
+    // console.log(res);
+    return maxValue;
+
+  }
+
+
+  breadFirst() {
+    // Checking for nodes
+    if (!this.root) { throw new Error('No Nodes in This Tree'); }
+    // create empty arr for results and queue
+    let results = [];
+    let queue = [];
+    // set the starting/current point to the root
+    let curr = this.root;
+    // pushing the current to the queue
+    queue.push(curr);
+    // start the traversal with a while there is a length
+    while (queue.length>0) {
+      // assigning current to shifted val
+      curr = queue.shift();
+      // pushing that node val to the results arr
+      results.push(curr.value);
+      // if truthy, push curr.left into queue
+      if (curr.left) queue.push(curr.left);
+      // if truthy, push curr.right into queue
+      if (curr.right) queue.push(curr.right);
+    }
+    return results;
+  }
+}
+
+
+const tree = new BT();
+
+let node1 = new Node(10);
+// let node2 = new Node(15);
+// let node3 = new Node(18);
+// let node4 = new Node(20);
+// let node5 = new Node(24);
+// let node6 = new Node(30);
+// let node7 = new Node(99);
+
+tree.root = node1;
+// tree.root.left = node2;
+// tree.root.right = node3;
+// tree.root.left.left = node4;
+// tree.root.left.right = node5;
+// tree.root.right.left = node6;
+// tree.root.right.right = node7;
+
+
+// tree.breadFirst();
+
+// console.log(JSON.stringify(tree));
+
+// let result = tree.findMax();
+
+// console.log(result);
+
+module.exports = BT;
+
---
+++ js-401/tree-breadth-first/README.md
@@ -0,0 +1,7 @@
+# tree-max
+
+## Feature
+Feature Tasks
+Write a function called breadth first
+Arguments: tree
+Return: list of all values in the tree, in the order they were encountered
