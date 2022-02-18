9328badadf0f4556cb096d764723c11ff2a5ec33
jamiguel23 <josematthewmiguel@gmail.com>
2022-02-18 11:10:40 -0800

    added hashtable file with linkedList class

---
+++ js-401/hashtable/README.md
@@ -0,0 +1,28 @@
+# hashtable
+
+
+## Challenge
+Implement a Hashtable Class with the following methods:
+
+- set
+Arguments: key, value
+Returns: nothing
+This method should hash the key, and set the key and value pair in the table, handling collisions as needed.
+Should a given key already exist, replace its value from the value argument given to this method.
+- get
+Arguments: key
+Returns: Value associated with that key in the table
+- contains
+Arguments: key
+Returns: Boolean, indicating if the key exists in the table already.
+- keys
+Returns: Collection of keys
+- hash
+Arguments: key
+Returns: Index in the collection for that key
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## API
+<!-- Description of each method publicly available in each of your hashtable -->
---
+++ js-401/hashtable/linkedList.js
@@ -0,0 +1,55 @@
+'use strict';
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    let node = new Node(value);
+    if(this.head === null) {
+      this.head = node;
+      return;
+    }
+
+    let currentNode = this.head;
+    while(currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = node;
+  }
+
+  insert(value) {
+
+    //define head
+    let node = new Node(value);
+
+
+    //make head to node
+    this.head = node;
+  }
+
+  includes(value){
+    //setting head to current node
+    let currentNode = this.head;
+
+    while (currentNode){
+      //validate if the nodes value is equal to the passed value
+      if (currentNode.value === value){
+        return true;
+      }
+      //moves to next node to validate with passed value
+      currentNode = currentNode.next;
+    }
+    //returns false if passed value is not found
+    return false;
+  }
+}
