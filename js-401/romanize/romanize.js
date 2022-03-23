e3df5d718233f774c275f2a846dd3f6111d690c5
Matt <josematthewmiguel@gmail.com>
2022-03-23 12:50:28 -0700

    worked on strech goal

--- js-401/romanize/romanize.js
+++ js-401/romanize/romanize.js
@@ -4,26 +4,62 @@ const convertToRoman = (num) => {
 
   var romanToNum = {
     M: 1000,
-    CM: 900,
+    // CM: 900,
     D: 500,
-    CD: 400,
+    // CD: 400,
     C: 100,
-    XC: 90,
+    // XC: 90,
     L: 50,
-    XL: 40,
+    // XL: 40,
     X: 10,
-    IX: 9,
+    // IX: 9,
     V: 5,
-    IV: 4,
+    // IV: 4,
     I: 1
   };
 
+
   let roman = '';
 
-  for (var i in romanToNum){
+  for (let i in romanToNum){
     console.log(i, romanToNum[i])
     while ( num >= romanToNum[i]){
 
+      if (num >= 900){
+        roman += 'CM'
+        num -= 900
+        break
+      }
+      if (num >= 400){
+        roman += 'CD'
+        num -= 400
+        break
+      }
+
+      if (num >= 90){
+        roman += 'XC'
+        num -= 90
+        break
+      }
+
+      if (num >= 40){
+        roman += 'XL'
+        num -= 40
+        break
+      }
+      if (num === 9){
+        roman += 'IX'
+        num -= 9
+        break
+      }
+
+      if (num === 4){
+        roman += 'IV'
+        num -= 4
+        break
+      }
+
+      console.log(num)
       roman += i
       num -= romanToNum[i];
       console.log('this is the new num after subtracting' , num)
@@ -33,6 +69,6 @@ const convertToRoman = (num) => {
   return roman
 }
 
-let result = convertToRoman(400);
+let result = convertToRoman(999);
 
 console.log('this is the result', result)
