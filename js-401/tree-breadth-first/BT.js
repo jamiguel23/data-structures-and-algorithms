9179f3c0ba3b11489f238ff2e85db7d15381b6c0
Matt <josematthewmiguel@gmail.com>
2022-04-14 21:05:21 -0700

    working on tree max value

--- js-401/tree-breadth-first/BT.js
+++ js-401/tree-breadth-first/BT.js
@@ -1,4 +1,5 @@
 'use strict';
+const treeify = require('treeify')
 
 const Node = require('./Node.js');
 
@@ -46,7 +47,7 @@ class BT {
 
   findMax() {
 
-    let maxValue = 0;
+    let maxValue = Number.NEGATIVE_INFINITY;
     // let res = [];
     let walk = (node) => {
       if (node.left) walk(node.left);
@@ -92,29 +93,29 @@ class BT {
 const tree = new BT();
 
 let node1 = new Node(10);
-// let node2 = new Node(15);
-// let node3 = new Node(18);
-// let node4 = new Node(20);
-// let node5 = new Node(24);
-// let node6 = new Node(30);
-// let node7 = new Node(99);
+let node2 = new Node(15);
+let node3 = new Node(18);
+let node4 = new Node(20);
+let node5 = new Node(24);
+let node6 = new Node(30);
+let node7 = new Node(99);
 
 tree.root = node1;
-// tree.root.left = node2;
-// tree.root.right = node3;
-// tree.root.left.left = node4;
-// tree.root.left.right = node5;
-// tree.root.right.left = node6;
-// tree.root.right.right = node7;
+tree.root.left = node2;
+tree.root.right = node3;
+tree.root.left.left = node4;
+tree.root.left.right = node5;
+tree.root.right.left = node6;
+tree.root.right.right = node7;
 
 
 // tree.breadFirst();
 
-// console.log(JSON.stringify(tree));
+console.log(treeify.asTree(tree), false);
 
-// let result = tree.findMax();
+let result = tree.findMax();
 
-// console.log(result);
+console.log(result);
 
 module.exports = BT;
 
