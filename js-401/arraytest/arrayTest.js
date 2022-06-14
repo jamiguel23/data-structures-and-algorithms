85c7428ceceb58d8257dc7c7340f0b54f8771b23
Matt <josematthewmiguel@gmail.com>
2022-06-14 12:14:13 -0700

    for loop running

--- js-401/arraytest/arrayTest.js
+++ js-401/arraytest/arrayTest.js
@@ -2,10 +2,20 @@
 
 Array = ['green', 'yellow', 'black', 'blue', 'red']
 
-function(arr) {
+function printFirstThree(arr) {
 
   let firstThree = [];
   let count = 0;
 
-  if ( count === 2 )
+  // if ( count < 2 ){
+    for ( let i = 0; i < arr.length; i++){
+      firstThree.push(arr[i]);
+      count++;
+      console.log('this is count' , count)
+    }
+  // }
+
+  return firstThree
 }
+
+console.log(printFirstThree(Array));
