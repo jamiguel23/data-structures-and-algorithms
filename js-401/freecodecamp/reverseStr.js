dabb0f63ebc3bfe91faff71f82ebd03cc0f2c587
Matt <josematthewmiguel@gmail.com>
2022-06-30 15:32:39 -0700

    revesed str

---
+++ js-401/freecodecamp/reverseStr.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let reverseStr = (str) => {
+
+  let reversed = '';
+
+  for (let i = str.length - 1; i>= 0; i--){
+
+    reversed += str[i];
+  }
+
+  return reversed
+
+}
+
+
+let string = 'hello'
+
+console.log(reverseStr(string));
+
