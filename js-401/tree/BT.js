86c4a990593246385caef6353fb2d6e2b3985529
Matt <josematthewmiguel@gmail.com>
2022-07-05 17:13:25 -0700

    added treeify npm package, finished invert tree method

--- js-401/tree/BT.js
+++ js-401/tree/BT.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Node = require('./Node.js');
+const treeify = require('treeify')
 
 
 
@@ -77,6 +78,7 @@ let node3 = new Node(18);
 let node4 = new Node(20);
 let node5 = new Node(24);
 let node6 = new Node(30);
+let node7 = new Node(50)
 
 // set nodes to positions of trees
 tree.root = node1;
@@ -85,9 +87,11 @@ tree.root.right = node3;
 tree.root.left.left = node4;
 tree.root.left.right = node5;
 tree.root.right.left = node6;
+tree.root.right.right = node7
 
 
-tree.preOrder();
+console.log('this is tree', tree)
+console.log('this is inverted tree', JSON.stringify(tree.invert()))
 // console.log('************');
 // tree.inOrder();
 // console.log('************');
