7f8df7d4be1480bf8774f1f083a028f0991edf6a
Matt <josematthewmiguel@gmail.com>
2022-07-23 16:03:15 -0700

    working on inverted tree

--- js-401/tree/BT.js
+++ js-401/tree/BT.js
@@ -75,23 +75,23 @@ const tree = new BT();
 let node1 = new Node(10);
 let node2 = new Node(15);
 let node3 = new Node(18);
-let node4 = new Node(20);
-let node5 = new Node(24);
-let node6 = new Node(30);
-let node7 = new Node(50)
+// let node4 = new Node(20);
+// let node5 = new Node(24);
+// let node6 = new Node(30);
+// let node7 = new Node(50)
 
 // set nodes to positions of trees
 tree.root = node1;
 tree.root.left = node2;
 tree.root.right = node3;
-tree.root.left.left = node4;
-tree.root.left.right = node5;
-tree.root.right.left = node6;
-tree.root.right.right = node7
+// tree.root.left.left = node4;
+// tree.root.left.right = node5;
+// tree.root.right.left = node6;
+// tree.root.right.right = node7
 
 
-console.log('this is tree', tree)
-console.log('this is inverted tree', JSON.stringify(tree.invert()))
+console.log('this is tree', JSON.stringify(tree))
+console.log('this is inverted tree', JSON.stringify(tree.invert()));
 // console.log('************');
 // tree.inOrder();
 // console.log('************');
